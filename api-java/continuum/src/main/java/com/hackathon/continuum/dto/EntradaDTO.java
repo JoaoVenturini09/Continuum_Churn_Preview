250cb7e6d82843037a77f3005264464373d42755
Nayara Calixto <nayaranec2@gmail.com>
2025-12-15 22:07:49 -0300

    Alteração dos nomes dos atributos

--- api-java/continuum/src/main/java/com/hackathon/continuum/dto/EntradaDTO.java
+++ api-java/continuum/src/main/java/com/hackathon/continuum/dto/EntradaDTO.java
@@ -6,9 +6,9 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record EntradaDTO(
     @JsonAlias ("account_Charges_Monthly") double valor_mensal_conta,
-    @JsonAlias ("internet_InternetService_Fiber optic") String tipo_servico_internet,
+    @JsonAlias ("internet_InternetService_Fiber_optic") String tipo_servico_internet,
     @JsonAlias ("account_PaperlessBilling") String tipo_faturamento,
-    @JsonAlias ("account_PaymentMethod_Electronic check") String metodo_pagamento,
+    @JsonAlias ("account_PaymentMethod_Electronic_check") String metodo_pagamento,
     @JsonAlias ("internet_StreamingMovies_1") String uso_streaming) {
 
 }
