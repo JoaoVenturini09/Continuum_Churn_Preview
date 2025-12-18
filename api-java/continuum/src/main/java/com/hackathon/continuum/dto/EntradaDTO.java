1ae7a87ac6d2e42889a60d677d578406b1532d5d
GabryelJ <gabryeljuliods@gmail.com>
2025-12-18 19:31:20 -0300

    Adiciona dependência Validation e ajusta imports

--- api-java/continuum/src/main/java/com/hackathon/continuum/dto/EntradaDTO.java
+++ api-java/continuum/src/main/java/com/hackathon/continuum/dto/EntradaDTO.java
@@ -2,6 +2,9 @@ package com.hackathon.continuum.dto;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record EntradaDTO(
